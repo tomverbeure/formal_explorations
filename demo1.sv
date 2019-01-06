f39390a1a67543b790db30f4d6e7d931e56d3faa
Tom Verbeure <hombre+github@gmail.com>
2019-01-05 20:22:14 -0800

    initstate.

---
+++ demo1.sv
@@ -0,0 +1,26 @@
+
+module demo1(
+    input clk
+    );
+
+    reg reset_;
+
+    initial reset_ = 1'b0;
+    always @(posedge clk) reset_ <= 1'b1;
+
+    reg [1:0] cntr;
+    always @(posedge clk) begin
+
+        if (cntr == 2)
+            cntr <= 0;
+        else
+            cntr <= cntr + 1;
+
+        if (reset_) begin
+            cntr <= 0;
+        end
+    end
+
+    assert property($initstate || !reset_ || cntr <= 2);
+    
+endmodule
